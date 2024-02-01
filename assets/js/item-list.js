f029079f1eab5ac541772fd268232779362dd684
Hirushat <hirusha10online@gmail.com>
2024-02-01 23:50:27 +0530

    add listItem

--- assets/js/item-list.js
+++ assets/js/item-list.js
@@ -1,6 +1,19 @@
-var itemList = [];
+const itemInput = document.getElementById("itemInput");
+const itemList = document.getElementById("itemList");
 
-let item = {
-	item : "Petya",
-  	age : 15
-};
+function clear(){
+	document.getElementById("itemInput").value = "";
+}
+
+function addItem(){	
+	const newItemText = itemInput.value;
+	const newItem = createListItem(newItemText);
+	itemList.appendChild(newItem);
+	clear();
+}
+
+function createListItem(text){
+	const listItem = document.createElement("li");
+	listItem.textContent = text;
+	return listItem;
+}
